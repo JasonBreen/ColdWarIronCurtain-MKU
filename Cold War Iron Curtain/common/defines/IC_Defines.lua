53c77e1aa150c3c9a8e12496bedc84d83012ef36
thatriverguy <59489248+PYA-II@users.noreply.github.com>
2026-06-15 03:24:37 +0300

    division designer fix pt.2

--- Cold War Iron Curtain/common/defines/IC_Defines.lua
+++ Cold War Iron Curtain/common/defines/IC_Defines.lua
@@ -97,8 +97,8 @@ NDefines.NDiplomacy.VOLUNTEERS_TRANSFER_SPEED = 7
 NDefines.NGame.END_DATE = "2050.5.23.12"
 NDefines.NGame.START_DATE = "1945.5.23.12"
 
-NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 2 -- Max width of support in division designer.
-NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 5 -- Max width of support in division designer.
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_WIDTH = 1 -- Max width of support in division designer.
+NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 10 -- Max width of support in division designer.
 NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 1000;
 NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 1000;
 NDefines.NMilitary.MAX_AIR_EXPERIENCE = 1000;
