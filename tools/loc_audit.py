68fbf948ee0f508f3d94ac6ebe91e5469e1b9ca2
afushin <140269429+afushin@users.noreply.github.com>
2026-08-11 13:01:15 -0500

    Further VIN-FRE Indochina Work
    - Increased VIN & FRE division limits
    - Fixed stale post-DBP battle Geneva Coference not triggering if NLF
      alive

--- tools/loc_audit.py
+++ tools/loc_audit.py
@@ -53,7 +53,7 @@ THEATRES = OrderedDict([
     ("VIE Audit - VIE Events.csv", ["VIE_Events.txt"]),
     ("SEA Audit - North Vietnam.csv", [
         "VIN_Events.txt", "VIN_FORPOL_events.txt", "VIN_MIL_events.txt",
-        "VIN_Campaign_Events.txt", "SWF_VIN_events.txt",
+        "VIN_Campaign_Events.txt", "VIN_Post_DBP_South_Events.txt", "SWF_VIN_events.txt",
         "SWF_VIN_second_congress_events_final.txt",
         "North_Vietnam_Flavor.txt", "North Vietnam.txt",
     ]),
@@ -95,6 +95,7 @@ SEA_LOC_FILES = [
     "VIN_events_l_english.yml", "VIN_focus_l_english.yml", "VIN_misc_l_english.yml",
     "VIN_FORPOL_l_english.yml", "VIN_MIL_l_english.yml", "VIN_newstuff_l_english.yml",
     "VIN_parties_l_english.yml", "VIN_custom_effect_tooltips_l_english.yml",
+    "VIN_Post_DBP_South_l_english.yml",
     "French_Indochina_l_english.yml", "FRE_CEFEO_l_english.yml",
     "IC_Laos_Raid_l_english.yml", "CWIC_Geneva_Conference_l_english.yml",
     "CWIC_Indochina_Outcomes_l_english.yml", "CWIC_Struggle_l_english.yml",
