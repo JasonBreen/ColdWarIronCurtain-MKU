5245261a5ee4cf9c837f741c64136ccd77e75a24
afushin <140269429+afushin@users.noreply.github.com>
2026-08-13 16:13:45 -0500

    Implement Operations Pollux and Atlante

--- tools/loc_audit.py
+++ tools/loc_audit.py
@@ -61,6 +61,7 @@ THEATRES = OrderedDict([
         "FRE_Events.txt", "FRE_Operation_Events.txt", "Indochina_War.txt",
         "Indochina_War_Rework.txt", "Indochina_Flavor_Events.txt",
         "IC_Laos_Raid.txt", "FRE_Dien_Bien_Response_Events.txt",
+        "FRE_Pollux_Events.txt", "FRE_Atlante_Events.txt",
         "SWF_Indochina_War_events.txt",
     ]),
     ("SEA Audit - Geneva and Settlement.csv", [
@@ -99,6 +100,7 @@ SEA_LOC_FILES = [
     "VIN_Post_DBP_South_l_english.yml",
     "French_Indochina_l_english.yml", "FRE_CEFEO_l_english.yml",
     "FRE_Dien_Bien_Response_l_english.yml",
+    "FRE_events_l_english.yml", "FRE_focus_l_english.yml", "FRE_misc_l_english.yml",
     "IC_Laos_Raid_l_english.yml", "CWIC_Geneva_Conference_l_english.yml",
     "CWIC_Indochina_Outcomes_l_english.yml", "CWIC_Struggle_l_english.yml",
     "MEO_events_l_english .yml", "MEO_focus_50s_l_english.yml",
